8121482742e7b36cb7519bbbc4838f4d4e470c4f
Wiliane <wilianedomingos@gmail.com>
2022-09-08 20:02:52 -0400

    first commit

---
+++ src/br/com/dio/PrimeiroProgramaJava.java
@@ -0,0 +1,18 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroProgramaJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+		/*int a= 6;
+		int b= 5;
+		System.out.println("Hello World! "+ (a+b));*/
+	}
+
+}
